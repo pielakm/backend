0c73be9bdd0e1c7fd3f85176894a5ebe777cee4d
PewkaP <panplewka@gmail.com>
2024-10-18 18:12:29 +0200

    naprawiłem błąd

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT;
 // const users_controllers = express.Router()
 import users_controllers from "./routes/UsersRoutes"
 
-import  users_controllers  from "./routes/UsersRoutes"
 
 import { rateLimit } from "express-rate-limit"
 
