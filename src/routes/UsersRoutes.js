2fad8979a7cc958b609d6720fa38af8be224c2ee
PewkaP <panplewka@gmail.com>
2024-10-18 18:28:56 +0200

    funkcja lorem

--- src/routes/UsersRoutes.js
+++ src/routes/UsersRoutes.js
@@ -19,6 +19,9 @@ users_controllers.post("/users/read", UsersRead)
 users_controllers.put("/users/update/:id", UsersUpdate)
 users_controllers.delete("/users/delete", UsersDelete)
 users_controllers.get("/users/auth", UserAuth)
+users_controllers.get("/lorem",(req,res)=>{
+    res.send("Lorem ipsum dolor sit amet...")
+})
 
 
 export default users_controllers
