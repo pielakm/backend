f82cafbbe90a293c3c87765143b1b698ad8b6ba3
pielakm <s97704@pollub.edu.pl>
2024-10-25 16:31:33 +0200

    dodanie create/read/delete comment

--- src/models/Models.js
+++ src/models/Models.js
@@ -3,3 +3,4 @@ import { PrismaClient } from "@prisma/client"
 export const UsersModels = new PrismaClient().users
 export const EventsModels = new PrismaClient().events
 export const PaymentMethodModels = new PrismaClient().payment_methods
+export const CommentsModels = new PrismaClient().comments
