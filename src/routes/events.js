d41350811aab4d3cd1c0ab2dc754db772332e682
pielakm <s97704@pollub.edu.pl>
2024-11-06 09:18:51 +0100

    dodanie .get(/:id) 	modified:   src/routes/events.js

--- src/routes/events.js
+++ src/routes/events.js
@@ -24,4 +24,20 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const event = await prisma.events.findUnique({
+            where: { idevent: parseInt(id) }, // Ensure this matches your database field
+        });
+        if (event) {
+            res.status(200).json({ event });
+        } else {
+            res.status(404).json({ message: 'Event not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
 export default router;
